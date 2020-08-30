ee037fae0d51a579caecd80cbc902ca7e5843771
shin04 <daikon0413@gmail.com>
2020-08-30 09:07:05 +0900

    allow methods

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,12 @@ const api_key = env.API_KEY
 
 router.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
   );
+  
   next();
 });
 
